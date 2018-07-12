50f7d3ffe0e31afd437f96b0fff9128b3538ff70
juanmagalhaes <contato.juanmagalhaes@gmail.com>
2018-07-12 17:00:40 -0300

    Making testcase harder

--- src/questions/1/index.test.js
+++ src/questions/1/index.test.js
@@ -1,6 +1,6 @@
 import { stripQuoteFunctional, stripQuoteWithRegex } from "./";
 
-const quotedString = `sdjasd'asld"dskdja"sdjlaskjdalskj'dSDJKSD"JKSDLKSJD"`;
+const quotedString = `"s'''dj"""asd'asld"dskdja"sdjlaskjdalskj'dSDJKSD"JKSDLKSJD"'`;
 const unquotedString = "sdjasdaslddskdjasdjlaskjdalskjdSDJKSDJKSDLKSJD";
 
 describe("Question 1 => Function to strip quotes", () => {
